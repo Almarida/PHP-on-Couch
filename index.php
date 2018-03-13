6453511752ee609610766f33591b25e607bb7a4f
WeekendUpdate <corey.haggarty@gmail.com>
2018-03-13 16:57:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
-<?php
-$couch_dsn = "http://10.1.0.4:8091/";
-$couch_db = "songs";
- 
-require_once "./lib/couch.php";
-require_once "./lib/couchClient.php";
-require_once "./lib/couchDocument.php";
- 
- 
-$client = new couchClient($couch_dsn,$couch_db);
-?>
-try {
-    $info = $client->getDatabaseInfos();
-} catch (Exception $e) {
-    echo "Error:".$e->getMessage()." (errcode=".$e->getCode().")\n";
-    exit(1);
-}
-print_r($info);
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
