c4c2ffb955a068f9d4e7ff18628abf882312ec70
WeekendUpdate <corey.haggarty@gmail.com>
2018-03-13 16:46:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,3 +9,10 @@ require_once "./lib/couchDocument.php";
  
 $client = new couchClient($couch_dsn,$couch_db);
 ?>
+try {
+    $info = $client->getDatabaseInfos();
+} catch (Exception $e) {
+    echo "Error:".$e->getMessage()." (errcode=".$e->getCode().")\n";
+    exit(1);
+}
+print_r($info);
